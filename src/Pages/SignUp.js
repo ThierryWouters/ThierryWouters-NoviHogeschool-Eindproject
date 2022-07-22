94fb485de52ac448155527f4bbce602fe74c3279
ThierryWouters <thierry@thierrywouters.com>
2022-07-22 13:09:09 +0200

    signup form

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,9 +1,14 @@
 import React from "react";
 import './Pages Stylesheets/SignUp.css';
+import RegisterForm from "../Components/RegisterForm";
 
 function SignUp() {
     return (
+        <div className="main">
         <h4>Sign up here</h4>
+
+        <RegisterForm/>
+        </div>
     );
 }
 
